954390ad11c7abb5ebfaa8ca3034f02d1cab1194
sunghun <bbakkomm@gmail.com>
2024-10-13 22:33:43 +0900

    경로 수정

--- client/src/utils/customFetch.js
+++ client/src/utils/customFetch.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const customFetch = axios.create({
-  baseURL: 'https://port-0-testvercel-m26geil7668e23ca.sel4.cloudtype.app//api/v1',
+  baseURL: 'https://port-0-testvercel-m26geil7668e23ca.sel4.cloudtype.app/api/v1',
 });
 
 export default customFetch;
